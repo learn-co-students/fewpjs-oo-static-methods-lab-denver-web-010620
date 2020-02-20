a6348c2c000a883f66b07e35b0ea5dc04e056baf
mrshappy0 <ajs11300@gmail.com>
2020-02-19 23:03:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^- '\w]/g, "");
+  }
+
+  static titleize(string) {
+    let upper = string.replace(/(\b[a-z])/g, c => c.toUpperCase());
+    console.log(upper)
+    let andDown = upper.replace(
+      /((The|And|From|But|An|Of|A|For|At|By)((?=,)|(?= )))/g,
+      c => c.charAt(0).toLowerCase() + c.substring(1)
+      );
+    return (andDown.charAt(0).toUpperCase() + andDown.substring(1));
+  }
 }
+
+Formatter.titleize("a Tale of Two Cities");
